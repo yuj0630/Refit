361ba06620284691060f3131136feaf8b7836ab5
yuj0630 <un3561@naver.com>
2023-06-15 11:29:58 +0900

    design, card carousel

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def upload():
 
     return render_template('result.html', result=result)
 
+@app.route('/tospring')
+def spring():
+    return 'test'
 
-@app.route('/user')
-def user():
-    return 'Hello, User!'
 
 if __name__ == '__main__':
     app.run(debug=True)
